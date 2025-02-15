b73468213a2e0e0c39d3c44238867f112b9798b3
Cosmo André <soucosmomoraes@gmail.com>
2025-02-15 20:16:55 -0300

    now the path starts with slash

--- src/services/path_sanitize.rs
+++ src/services/path_sanitize.rs
@@ -4,11 +4,14 @@ use regex::Regex;
 pub fn path_sanitize(path: Path<String>) -> Result<String, &'static str> {
     let re = Regex::new(r"^[a-zA-Z0-9]+(?:[/-][a-zA-Z0-9]+)*$").unwrap();
 
-    dbg!(&path);
-
     if !re.is_match(&path) {
         return Err("Invalid path");
     }
 
-    Ok(path.into_inner())
+    Ok(
+        format!(
+            "/{}",
+            path.into_inner()
+        )
+    )
 }
