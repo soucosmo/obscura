d7883355e37d9f1a3e8d6fcc44df46862d91da26
Cosmo André <soucosmomoraes@gmail.com>
2025-02-15 23:23:25 -0300

    Optimized token comparison with is_root

--- src/services/verify_token.rs
+++ src/services/verify_token.rs
@@ -2,7 +2,7 @@ use crate::dao::{Token, VerifyToken};
 
 
 pub async fn verify_token(token: &Token, path: &str, write: bool) -> VerifyToken {
-    if token.is_root || token.paths.contains_key("/") {
+    if token.is_root {
         return VerifyToken::Allowed;
     }
 
