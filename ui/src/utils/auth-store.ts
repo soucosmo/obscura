7bfc1fc9711016eb00b39bf22aaf9e55b820ee0e
Cosmo André <soucosmomoraes@gmail.com>
2025-02-20 23:12:35 -0300

    added authentication state

--- ui/src/utils/auth-store.ts
+++ ui/src/utils/auth-store.ts
@@ -3,33 +3,42 @@ import { API_URL } from "./config"
 import { create } from "zustand"
 
 interface AuthState {
-  rootTokenExists?: boolean;
-  token: string;
-  isAuthenticated: boolean;
-  ready: boolean;
-  paths: string[];
-  login: (token: string) => void;
-  logout: () => void;
-  generateRootToken: () => void;
-  checkRootTokenExists: () => void;
+    rootTokenCreating: boolean
+    rootTokenExists?: boolean
+    token: string
+    isAuthenticated: boolean
+    paths: string[]
+    login: (token: string) => void
+    logout: () => void
+    generateRootToken: () => void
+    checkRootTokenExists: () => void
 }
 
 export const useAuthStore = create<AuthState>((set) => {
-    const token = localStorage.getItem("token") ?? "";
-  
+    const token = localStorage.getItem("token") ?? ""
+    const rootTokenCreating = localStorage.getItem('rootTokenCreating') == 'true'
+
     return {
+        rootTokenCreating,
         rootTokenExists: undefined,
         token,
-        ready: false,
         isAuthenticated: !!token,
         paths: [],
         checkRootTokenExists: () => {
-            console.log('faznd consulta')
-
             fetch(`${API_URL}/api/token/root-exists`)
                 .then((res) => {
-                    console.log('reees', res.status)
-                    set({ rootTokenExists: res.status == 200 })
+                    const rootTokenExists = res.status == 200
+
+                    let rootTokenCreating = true
+
+                    if (!rootTokenExists && !localStorage.getItem('rootTokenCreating')) {
+                        localStorage.setItem("rootTokenCreating", "true")
+                    }
+
+                    set({
+                        rootTokenExists,
+                        rootTokenCreating,
+                    })
                 })
         },
         generateRootToken: () => {
@@ -44,6 +53,8 @@ export const useAuthStore = create<AuthState>((set) => {
                 }).then((data) => {
                     console.log(data)
                     if (data.status === 201) {
+                        localStorage.setItem("rootTokenCreating", "true")
+
                         set({
                             rootTokenExists: true,
                             token: data.text,
@@ -53,17 +64,18 @@ export const useAuthStore = create<AuthState>((set) => {
         },
         login: async (token) => {
             fetch(`${API_URL}/api/token/${token}`).then(async (res) => {
-                console.log('res', res)
                 if (res.status == 200) {
-                    localStorage.setItem("token", token);
+                    localStorage.setItem("token", token)
+
+                    localStorage.setItem("rootTokenCreating", "false")
 
                     let info = await res.json()
 
                     set({
                         token,
                         isAuthenticated: true,
-                        ready: true,
                         paths: info.paths,
+                        rootTokenCreating: false,
                     })
 
                     toast.success('the token has been successfully verified!')
@@ -75,9 +87,9 @@ export const useAuthStore = create<AuthState>((set) => {
         logout: () => {
             set({
                 isAuthenticated: false,
-                ready: false,
                 token: "",
                 paths: [],
+                rootTokenCreating: false,
             })
 
             localStorage.removeItem("token")
