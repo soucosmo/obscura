76ce5b2a5f831c42dc0c1d0a18b49bd6b998a843
Cosmo André <soucosmomoraes@gmail.com>
2025-02-23 16:12:54 -0300

    Improvements to the navigation system and component display

--- ui/src/layout.tsx
+++ ui/src/layout.tsx
@@ -1,14 +1,18 @@
 import { Navbar } from "./components/navbar"
+import { Footer } from "./components/footer"
 
 type LayoutProps = {
-    children: React.ReactNode;
+    children: React.ReactNode | null
 }
 
 export const Layout = ({ children }: LayoutProps) => {
     return (
         <>
             <Navbar />
-            {children}
+                <div className="overflow-x-auto min-h-[85vh] my-8">
+                    {children}
+                </div>
+            <Footer />
         </>
     )
 }
