181efd15cfeeaf2fbd33ed294c051a17ae8e3ba2
Cosmo André <soucosmomoraes@gmail.com>
2025-02-23 16:06:52 -0300

    Default storage path has been changed

--- src/dao/config.rs
+++ src/dao/config.rs
@@ -6,4 +6,4 @@ pub const PREFIX_API: &str = "/api";
 
 pub const DEFAULT_PORT: u16 = 9797;
 pub const DEFAULT_HOST: &str = "127.0.0.1";
-pub const DEFAULT_KEYSPACE: &str = "../.obscura";
+pub const DEFAULT_KEYSPACE: &str = "/opt/";
