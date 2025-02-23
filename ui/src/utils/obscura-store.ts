7ffb3ce1f5ab49416b13c0da8bd20bb97519c1e7
Cosmo André <soucosmomoraes@gmail.com>
2025-02-23 16:11:09 -0300

    Added API calls and removed unused code

--- ui/src/utils/obscura-store.ts
+++ ui/src/utils/obscura-store.ts
@@ -1,46 +1,85 @@
-import { create } from "zustand";
+import { toast } from "react-toastify"
+import { API_URL } from "./config"
+import { create } from "zustand"
 
 interface ObscuraState {
-    isAuhenticated: boolean;
-    token: string;
-    rootTokenExists: boolean;
-    setToken: (token: string) => void;
-    checkToken: () => void;
-    clearToken: () => void;
-    checkRootToken: () => void;
+    page: string,
+    setPage: (page: string) => void
+    pathWithPrefix: object
+    setPathWithPrefix: (pathWithPrefix: any) => void
+    currentPath: string
+    setCurrentPath: (currentPath: string) => void
+    getPathsWithPrefix: (path: string) => void
+    pathContent: object
+    setPathContent: (pathContent: object) => void
+    saveConfigMap: () => void
+    pathEditing: boolean
+    setPathEditing: (pathEditing: boolean) => void
+    navigateToConfigMaps: () => void
 }
 
-export const useObscuraStore = create<ObscuraState>((set, get) => ({
-  rootTokenExists: false,
-  isAuhenticated: false,
-  token: "",
-  checkToken: () => {
-        const token = localStorage.getItem("token")
+export const useObscuraStore = create<ObscuraState>((set, get) => {
+    return {
+        page: 'ConfigMaps',
+        pathWithPrefix: {},
+        currentPath: '',
+        pathContent: {},
+        pathEditing: false,
+        setPathEditing: (pathEditing: boolean) => {
+            set({ pathEditing })
+        },
+        setPage: (page: string) => {
+            set({ page })
+        },
+        setPathWithPrefix: (pathWithPrefix) => {
+            set({ pathWithPrefix })
+        },
+        setCurrentPath: (currentPath: string) => {
+            set({ currentPath })
+        },
+        getPathsWithPrefix: (path: string) => {
+            fetch(`${API_URL}/api/config-map/${path}/prefix`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            }).then(async (res) => {
+                if (res.status == 200) {
+                    let paths = await res.json()
 
-        if (token) {
-        set({ token })
+                    set({ pathWithPrefix: paths })
+                } else {
+                    toast.error(await res.text())
+                }
+            })
+        },
+        setPathContent: (pathContent: object) => {
+            set({ pathContent })
+        },
+        saveConfigMap: () => {
+            const path = get().currentPath.substring(1, get().currentPath.length)
+
+            fetch(`${API_URL}/api/config-map/${path}`, {
+                method: 'put',
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(get().pathContent)
+            }).then(async (res) => {
+                if (res.status == 200) {
+                    toast.success('The config map has been successfully saved!')
+                } else {
+                    toast.error(await res.text())
+                }
+            })
+        },
+        navigateToConfigMaps: () => {
+            set({
+                page: 'ConfigMaps',
+                pathContent: {},
+                pathEditing: false,
+                currentPath: '',
+            })
         }
-
-
-        get().checkRootToken()
-  },
-  setToken: (token: string) => {
-    localStorage.setItem("token", token);
-
-        set({ token });
-  },
-  clearToken: () => {
-        localStorage.removeItem("token");
-        set({ token: "" });
-  },
-  checkRootToken: () => {
-      return;
-    if (get().rootTokenExists) {
     }
-
-    fetch("/api/token/root-exists")
-        .then((res) => {
-            set({ rootTokenExists: res.status != 200 })
-        })
-  }
-}));
+});
